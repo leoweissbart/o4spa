d798471987ad37a36460424dd5c93db382eb20f9
Leo <leo.weissbart@gmail.com>
2018-06-26 12:36:57 +0900

    changed to match fpga attack

--- correlation_vs_model.py
+++ correlation_vs_model.py
@@ -4,65 +4,77 @@ from pylab import *
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 #takes the filtered data and the trigger signal for entire SCM and the 2 model of pattern to return levels detected on the trace
-def correlation_vs_model_v2_2(data,trigger,modelH,modelL):
+def correlation_vs_model_v3(data,trigger,modelH1,modelH2,modelL):
     i,it=0,0
     allseg,seg=[],[]
-    while trigger[it]<3.5:#we use the trigger to find the beginning of ML algorithm
+    while trigger[it]>-2:#we use the trigger to find the beginning of ML algorithm
         it+=1
+    it+=3243#to remove the  part before the ladderstep
     j=it
     n_points=0
-    while trigger[j]>3.5:
+    while trigger[j]<-2:
         n_points+=1
         j+=1
     i=int(it)
     for k in range(256):
-        for j in range(len(modelH)):
+        for j in range(len(modelH1)):
             i=int(it)
             seg.append(data[i])
             it+=1
-        it+=(n_points/256)-len(modelH)#during ML algorithm n_points are captured and there is 256 laddersteps
+        it+=(n_points/256)-len(modelH1)+0.15#during ML algorithm n_points are captured and there is 256 laddersteps
         seg-=np.mean(seg)
         allseg.append(seg)
         seg=[]
+    
+    # ti=np.linspace(0,len(allseg[0]-1),len(allseg[0]))
+    # plt.figure()
+    # for i in range(len(allseg)):
+    #     plt.plot(ti,allseg[i])
+    # plt.show()
 
-    array_corrH,array_corrL=[],[]
-    t=np.linspace(0,len(modelH)-1,len(modelH))
+    array_corrH1,array_corrH2,array_corrL=[],[],[]
+    t=np.linspace(0,len(modelH1)-1,len(modelH1))
     for i in range(len(allseg)):
         # if i>202:
         # plt.figure()
         # plt.plot(t,allseg[i],'#1f77b4')
+        # plt.show()
 
         # plt.figure()
-        # plt.plot(t,allseg[i],t,modelH,'b',t,modelL,'r')
+        # plt.plot(t,allseg[i],t,modelH1,'b',t,modelL,'r')
         # plt.show()
         corrL=np.correlate(allseg[i],modelL)
-        corrH=np.correlate(allseg[i],modelH)
+        corrH1=np.correlate(allseg[i],modelH1)
+        corrH2=np.correlate(allseg[i],modelH2)
         array_corrL.append(corrL)
-        array_corrH.append(corrH)
+        array_corrH1.append(corrH1)
+        array_corrH2.append(corrH2)
 
-    t=np.linspace(0,len(array_corrH)-1,len(array_corrH))
+    t=np.linspace(0,len(array_corrH1)-1,len(array_corrH1))
     plt.figure()
     plt.title('Correlation')
-    plt.plot(t,array_corrL,'or',t,array_corrH,'ob',t,array_corrL,'r',t,array_corrH,'b')
+    plt.plot(t,array_corrL,'or',t,array_corrH1,'ob',t,array_corrH2,'ok',t,array_corrL,'r',t,array_corrH1,'b',t,array_corrH2,'k')
     plt.xlabel('Time (in Second)')
     plt.ylabel('Correlation level')
     plt.grid()
-    plt.legend(('Correlation with model A','Correlation with model B'),loc='best')
+    plt.legend(('Correlation with model L','Correlation with model H1','Correlation with model H2'),loc='best')
 
-    array_corrH-=np.mean(array_corrH)
+    array_corrH1-=np.mean(array_corrH1)
+    array_corrH2-=np.mean(array_corrH2)
     array_corrL-=np.mean(array_corrL)
 
-    t=np.linspace(0,len(array_corrH)-1,len(array_corrH))
+    t=np.linspace(0,len(array_corrH1)-1,len(array_corrH1))
     fig=plt.figure()
     ax = fig.gca()
     ax.set_xticks(np.arange(0, 256, 1))
     plt.title('Correlation')
     plt.scatter(t,array_corrL)
-    plt.scatter(t,array_corrH)
-    plt.plot(t,array_corrL,'#1f77b4',t,array_corrH,'#ff7f0e')
+    plt.scatter(t,array_corrH1)
+    plt.scatter(t,array_corrH2)
+    plt.plot(t,array_corrL,'#1f77b4',t,array_corrH1,'#ff7f0e',t,array_corrH2,'#009900')
     plt.xlabel('Number of ladderstep')
     plt.ylabel('Correlation level')
-    plt.legend(('Correlation with model A','Correlation with model B'),loc='best')
+    plt.legend(('Correlation with model L','Correlation with model H1','Correlation with model H2'),loc='best')
 
     ax = subplot(111)
 
@@ -73,10 +85,53 @@ def correlation_vs_model_v2_2(data,trigger,modelH,modelL):
     ax.xaxis.grid(True,'major',linewidth=2)
 
     level=[]
-    for i in range(len(array_corrH)):
-        if array_corrH[i]>array_corrL[i]:
-            level.append(1)
-        elif array_corrH[i]<array_corrL[i]:
+    #getting level v1
+    # for i in range(len(array_corrH1)):
+    #     if array_corrH1[i]>array_corrL[i]:
+    #         level.append(1)
+    #     elif array_corrH1[i]<array_corrL[i]:
+    #         level.append(0)
+    #     elif array_corrH2[i]>array_corrL[i]:
+    #         level.append(1)
+    #     elif array_corrH2[i]<array_corrL[i]:
+    #         level.append(0)
+    #     elif array_corrH1[i]>array_corrH2[i]:
+    #         level.append(1)
+    #     elif array_corrH1[i]<array_corrH2[i]:
+    #         level.append(1)
+    #     else:
+    #         level.append(2)#if there is problem with comparaison
+    #getting level v2, the assumption is that when corr is higher for H1 or H2 it cannot be the same next point, so we take the next higher level of correlation
+    #wasH1 raise when H1 is higher than the TWO other patterns and raise down when H2 is the higher level. H2 respectively with H1
+    wasH1,wasH2=False,False
+    for i in range(len(array_corrH1)):
+        if array_corrH1[i]>array_corrL[i]:
+            if wasH1==False:
+                level.append(1)
+                wasH1=True
+                wasH2=False
+            else:
+                if array_corrH2[i]>array_corrL[i]:
+                    level.append(1)
+                    wasH2=True
+                    wasH1=False
+                else:
+                    level.append(0)
+        elif array_corrH2[i]>array_corrL[i]:
+            if wasH2==False:
+                level.append(1)
+                wasH2=True
+                wasH1=False
+            else:
+                if array_corrH1[i]>array_corrL[i]:
+                    level.append(1)
+                    wasH1=True
+                    wasH2=False
+                else:
+                    level.append(0)
+        elif array_corrL[i]>array_corrH2[i]:
+            level.append(0)
+        elif array_corrL[i]>array_corrH1[i]:
             level.append(0)
         else:
             level.append(2)#if there is problem with comparaison
