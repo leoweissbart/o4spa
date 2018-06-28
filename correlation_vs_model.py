ce2c050eaaa4c4b0431ab28bac7a0cea6ab73a27
Leo <leo.weissbart@gmail.com>
2018-06-28 13:59:24 +0900

    better weighting for ladderstep cut and z-score normalization for each ladderstep

--- correlation_vs_model.py
+++ correlation_vs_model.py
@@ -9,20 +9,22 @@ def correlation_vs_model_v3(data,trigger,modelH1,modelH2,modelL):
     allseg,seg=[],[]
     while trigger[it]>-2:#we use the trigger to find the beginning of ML algorithm
         it+=1
-    it+=3243#to remove the  part before the ladderstep
+    it+=3243#to don't consider the  part before the laddersteps (3243 seems to be better than 3263 for some reason by looking at the correlation level that is not "constant")
     j=it
     n_points=0
     while trigger[j]<-2:
         n_points+=1
         j+=1
     i=int(it)
+    print(n_points/256,len(modelH1))
+    #we separate all the laddersteps in the dataset into 'allseg'
     for k in range(256):
         for j in range(len(modelH1)):
             i=int(it)
             seg.append(data[i])
             it+=1
-        it+=(n_points/256)-len(modelH1)+0.15#during ML algorithm n_points are captured and there is 256 laddersteps
-        seg-=np.mean(seg)
+        it+=(n_points/256)-len(modelH1)+(2*(3263-3243)/256)#weighting to always fit with the laddersteps
+        seg=(seg-np.mean(seg))/(np.std(seg))#normalization by Z-score of the segments
         allseg.append(seg)
         seg=[]
     
@@ -32,6 +34,7 @@ def correlation_vs_model_v3(data,trigger,modelH1,modelH2,modelL):
     #     plt.plot(ti,allseg[i])
     # plt.show()
 
+    #compute correlation for every models
     array_corrH1,array_corrH2,array_corrL=[],[],[]
     t=np.linspace(0,len(modelH1)-1,len(modelH1))
     for i in range(len(allseg)):
@@ -59,9 +62,9 @@ def correlation_vs_model_v3(data,trigger,modelH1,modelH2,modelL):
     plt.grid()
     plt.legend(('Correlation with model L','Correlation with model H1','Correlation with model H2'),loc='best')
 
-    array_corrH1-=np.mean(array_corrH1)
-    array_corrH2-=np.mean(array_corrH2)
-    array_corrL-=np.mean(array_corrL)
+    # array_corrH1-=np.mean(array_corrH1)
+    # array_corrH2-=np.mean(array_corrH2)
+    # array_corrL-=np.mean(array_corrL)
 
     t=np.linspace(0,len(array_corrH1)-1,len(array_corrH1))
     fig=plt.figure()
@@ -85,22 +88,6 @@ def correlation_vs_model_v3(data,trigger,modelH1,modelH2,modelL):
     ax.xaxis.grid(True,'major',linewidth=2)
 
     level=[]
-    #getting level v1
-    # for i in range(len(array_corrH1)):
-    #     if array_corrH1[i]>array_corrL[i]:
-    #         level.append(1)
-    #     elif array_corrH1[i]<array_corrL[i]:
-    #         level.append(0)
-    #     elif array_corrH2[i]>array_corrL[i]:
-    #         level.append(1)
-    #     elif array_corrH2[i]<array_corrL[i]:
-    #         level.append(0)
-    #     elif array_corrH1[i]>array_corrH2[i]:
-    #         level.append(1)
-    #     elif array_corrH1[i]<array_corrH2[i]:
-    #         level.append(1)
-    #     else:
-    #         level.append(2)#if there is problem with comparaison
     #getting level v2, the assumption is that when corr is higher for H1 or H2 it cannot be the same next point, so we take the next higher level of correlation
     #wasH1 raise when H1 is higher than the TWO other patterns and raise down when H2 is the higher level. H2 respectively with H1
     wasH1,wasH2=False,False
