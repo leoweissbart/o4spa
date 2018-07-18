831b53efeab45b49bfe96e0bdc6e8946652a843f
Leo <leo.weissbart@gmail.com>
2018-07-18 17:17:03 +0900

    enhanced cut of patterns in mesured traces

--- correlation_vs_model.py
+++ correlation_vs_model.py
@@ -9,7 +9,7 @@ def correlation_vs_model(data,trigger,modelH1,modelH2,modelL):
     allseg,seg=[],[]
     while trigger[it]>-2:#we use the trigger to find the beginning of ML algorithm
         it+=1
-    it+=3243#to don't consider the  part before the laddersteps (3243 seems to be better than 3263 for some reason by looking at the correlation level that is not "constant")
+    it+=3263#to don't consider the  part before the laddersteps (3243 seems to be better than 3263 for some reason by looking at the correlation level that is not "constant")
     j=it
     n_points=0
     while trigger[j]<-2:
@@ -23,7 +23,7 @@ def correlation_vs_model(data,trigger,modelH1,modelH2,modelL):
             i=int(it)
             seg.append(data[i])
             it+=1
-        it+=(n_points/256)-len(modelH1)+(2*(3263-3243)/256)#weighting to always fit with the laddersteps
+        it+=1.13#weighting to always fit with the laddersteps
         seg=(seg-np.mean(seg))/(np.std(seg))#normalization by Z-score of the segments
         allseg.append(seg)
         seg=[]
@@ -105,4 +105,109 @@ def correlation_vs_model(data,trigger,modelH1,modelH2,modelL):
             level.append(0)
         else:
             level.append(2)#if there is problem with comparaison
+    return level
+
+
+#with 4 patterns
+def correlation_vs_model_v2(data,trigger,modelH1,modelH2,modelL1,modelL2):
+    i,it=0,0
+    allseg,seg=[],[]
+    while trigger[it]>-2:#we use the trigger to find the beginning of ML algorithm
+        it+=1
+    it+=3243#to don't consider the  part before the laddersteps (3243 seems to be better than 3263 for some reason by looking at the correlation level that is not "constant")
+    j=it
+    n_points=0
+    while trigger[j]<-2:
+        n_points+=1
+        j+=1
+    i=int(it)
+    # print(n_points/256,len(modelH1))
+    #we separate all the laddersteps in the dataset into 'allseg'
+    for k in range(256):
+        for j in range(len(modelH1)):
+            i=int(it)
+            seg.append(data[i])
+            it+=1
+        it+=(n_points/256)-len(modelH1)+(2*(3263-3243)/256)#weighting to always fit with the laddersteps
+        seg=(seg-np.mean(seg))/(np.std(seg))#normalization by Z-score of the segments
+        allseg.append(seg)
+        seg=[]
+
+    #compute correlation for every models
+    array_corrH1,array_corrH2,array_corrL1,array_corrL2=[],[],[],[]
+    t=np.linspace(0,len(modelH1)-1,len(modelH1))
+    for i in range(len(allseg)):
+        corrL1=np.correlate(allseg[i],modelL1)
+        corrL2=np.correlate(allseg[i],modelL2)
+        corrH1=np.correlate(allseg[i],modelH1)
+        corrH2=np.correlate(allseg[i],modelH2)
+        array_corrL1.append(corrL1)
+        array_corrL2.append(corrL2)
+        array_corrH1.append(corrH1)
+        array_corrH2.append(corrH2)
+
+    t=np.linspace(0,len(array_corrH1)-1,len(array_corrH1))
+    plt.figure()
+    plt.title('Correlation')
+    plt.plot(t,array_corrL1,'or',t,array_corrL2,'og',t,array_corrH1,'ob',t,array_corrH2,'ok',t,array_corrL1,'r',t,array_corrL2,'g',t,array_corrH1,'b',t,array_corrH2,'k')
+    plt.xlabel('Time (in Second)')
+    plt.ylabel('Correlation level')
+    plt.grid()
+    plt.legend(('A1','A2','B1','B2'),loc='best')
+
+
+    # t=np.linspace(0,len(array_corrH1)-1,len(array_corrH1))
+    # fig=plt.figure()
+    # ax = fig.gca()
+    # ax.set_xticks(np.arange(0, 256, 1))
+    # plt.title('Correlation')
+    # plt.scatter(t,array_corrL)
+    # plt.scatter(t,array_corrH1)
+    # plt.scatter(t,array_corrH2)
+    # plt.plot(t,array_corrL,'#1f77b4',t,array_corrH1,'#ff7f0e',t,array_corrH2,'#009900')
+    # plt.xlabel('Number of ladderstep')
+    # plt.ylabel('Correlation level')
+    # plt.legend(('Correlation with model L','Correlation with model H1','Correlation with model H2'),loc='best')
+
+    # ax = subplot(111)
+
+    # ax.xaxis.set_major_locator(MultipleLocator(50))
+    # ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
+    # ax.xaxis.set_minor_locator(MultipleLocator(1))
+    # ax.xaxis.grid(True,'minor')
+    # ax.xaxis.grid(True,'major',linewidth=2)
+
+    level=[]
+    #getting level v2, the assumption is that when corr is higher for H1 or H2 it cannot be the same next point, so we take the next higher level of correlation
+    #wasH1 raise when H1 is higher than the TWO other patterns and raise down when H2 is the higher level. H2 respectively with H1
+    wasH1,wasH2=False,False
+    for i in range(len(array_corrH1)):
+        if array_corrH1[i]==max([array_corrH1[i],array_corrH2[i],array_corrL1[i],array_corrL2[i]]):
+            if wasH1==False:
+                level.append(1)
+                wasH1=True
+                wasH2=False
+            elif array_corrH2[i]==max([array_corrH2[i],array_corrL1[i],array_corrL2[i]]):
+                if wasH2==False:
+                    level.append(1)
+                    wasH1=False
+                    wasH2=True
+            else:
+                level.append(0)
+        elif array_corrH2[i]==max([array_corrH1[i],array_corrH2[i],array_corrL1[i],array_corrL2[i]]):
+            if wasH2==False:
+                level.append(1)
+                wasH1=False
+                wasH2=True
+            elif array_corrH1[i]==max([array_corrH1[i],array_corrL1[i],array_corrL2[i]]):
+                if wasH1==False:
+                    level.append(1)
+                    wasH1=True
+                    wasH2=False
+            else:
+                level.append(0)
+        elif array_corrL1[i]==max([array_corrH1[i],array_corrH2[i],array_corrL1[i],array_corrL2[i]]):
+            level.append(0)
+        elif array_corrL2[i]==max([array_corrH1[i],array_corrH2[i],array_corrL1[i],array_corrL2[i]]):
+            level.append(0)
     return level
--- patternClassification.py
+++ patternClassification.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 #returns High and Low with the traces of respectively non-zero and zero multiplication
 #data is the complete power trace
 #ladderstep is high for every calculation and is used to cut traces
@@ -8,12 +9,18 @@ import matplotlib.pyplot as plt
 #this version of the function evaluate the length of one ladderstep and cut the signal in 256. The length of the 256 laddersteps is given by the trigger and then we divide 
 #the number of points for the total time the trigger is ON by 256 and define this number as the length of one ladderstep.
 def patternClassification(data,trigger,level):
+    
     alt=False#used to separate the two patterns for high level
     m=[]#trace of 256 laddersteps
     High1,High2,Low=[],[],[]#in fact, after a few tries, it appeared there is 2 patterns for High
+
+    idebut= time.time()
     for i in range(len(data)):
         if trigger[i] < -2:
             m.append(data[i])
+    ifin=time.time()
+    print("remove time = ",ifin-idebut)
+
     del m[:3263]#remove the points before laddersteps. This nb have been adjusted regarding the curves obtained after patternclassification
     
     #plot m
@@ -98,3 +105,163 @@ def patternClassification(data,trigger,level):
             High2[i].pop()
 
     return Low,High1,High2
+
+
+#4patterns
+def patternClassification_v2(data,trigger,level):
+    state = 1
+    alt=False#used to separate the two patterns for high level
+    m=[]#trace of 256 laddersteps
+    High1,High2,Low1,Low2=[],[],[],[]#in fact, after a few tries, it appeared there is 2 patterns for High
+
+    idebut= time.time()
+    for i in range(len(data)):
+        if trigger[i] < -2:
+            m.append(data[i])
+    ifin=time.time()
+    print("remove time = ",ifin-idebut)
+
+    del m[:3263]#remove the points before laddersteps. This nb have been adjusted regarding the curves obtained after patternclassification
+    
+    #plot m
+    # tm=np.linspace(0,len(m)-1,len(m))
+    # plt.figure()
+    # plt.plot(tm,m)
+
+    ladderstep=[]
+    k=0#indenter to go through 256 loops of ladderstep
+    j=0#indenter to capture one ladderstep in 'ladderstep'
+    run=1#semaphore to separate repplishing of 'ladderstep' and the classification with respect to 'level'
+    len_ladderstep=len(m)/256#the number of points for one ladderstep
+    print("len(ladderstep = )",len_ladderstep)
+    i=0
+    petit=0
+    while i<len(m):
+        if run==1:
+            if j<len_ladderstep-1:
+                ladderstep.append(m[int(i)])
+                j+=1
+            else:
+                run=0
+                j=0
+                petit+=0.13#because all laddersteps must be the same length we have to add a little time "petit" 
+                #that will skip points to fit with the real time laddersteps. This parameter should be evaluated 
+                #manualy as we don't know the duration of one ladderstep beforehand, but this parameter will be 
+                #the same for every SCM.
+        if run==0:
+            run=1
+            # print(k)
+            # if k==0:
+            # print("level[",k,"]= ",level[k])
+            # t=np.linspace(0,len_ladderstep-1,len_ladderstep)
+            # plt.figure()
+            # plt.plot(t,ladderstep)
+            # plt.show()
+            if level[k]==0:
+                if state==1:
+                    Low1.append(ladderstep)
+                    state=1
+                elif state==2:
+                    Low2.append(ladderstep)
+                    state=3
+                elif state==3:
+                    Low2.append(ladderstep)
+                    state=3
+                elif state==4:
+                    Low1.append(ladderstep)
+                    state=1
+                else:
+                    print("f...")
+            elif level[k]==1:
+                if state==1:
+                    High1.append(ladderstep)
+                    state=2
+                elif state==2:
+                    High2.append(ladderstep)
+                    state=4
+                elif state==3:
+                    High2.append(ladderstep)
+                    state=4
+                elif state==4:
+                    High1.append(ladderstep)
+                    state=2
+                else:
+                    print("f...")
+            else:
+                raise ValueError('The values of level should be 0s or 1s.')
+            ladderstep=[]
+            k+=1
+        i+=1+petit
+        petit=0
+    if k<256:#important if the last ladderstep is shorter than len_ladderstep else we would lost the last bit
+        meanm=np.mean(ladderstep)
+        while len(ladderstep)<len_ladderstep-1:#artifficial padding with the mean value of the signal but the best would be to add the values from data
+            ladderstep.append(meanm)
+        if level[k]==0:
+            if state==1:
+                Low1.append(ladderstep)
+                state=1
+            elif state==2:
+                Low2.append(ladderstep)
+                state=3
+            elif state==3:
+                Low2.append(ladderstep)
+                state=3
+            elif state==4:
+                Low1.append(ladderstep)
+                state=1
+            else:
+                print("f...")
+        elif level[k]==1:
+            if state==1:
+                High1.append(ladderstep)
+                state=2
+            elif state==2:
+                High2.append(ladderstep)
+                state=4
+            elif state==3:
+                High2.append(ladderstep)
+                state=4
+            elif state==4:
+                High1.append(ladderstep)
+                state=2
+            else:
+                print("f...")
+        else:
+            raise ValueError('The values of level should be 0s or 1s.')
+
+    #we crop the sets so every sample are same length
+    lref=len(Low1[0])
+    for i in range(len(Low1)):
+        if len(Low1[i])<lref:
+            lref=len(Low1[i])
+            for j in range(i):
+                Low1[j].pop()
+        if len(Low1[i])>lref:
+            Low1[i].pop()
+    lref=len(Low2[0])
+    for i in range(len(Low2)):
+        if len(Low2[i])<lref:
+            lref=len(Low2[i])
+            for j in range(i):
+                Low2[j].pop()
+        if len(Low2[i])>lref:
+            Low2[i].pop()
+    lref=len(High1[0])
+    for i in range(len(High1)):
+        if len(High1[i])<lref:
+            lref=len(High1[i])
+            for j in range(i):
+                High1[j].pop()
+        if len(High1[i])>lref:
+            High1[i].pop()
+    lref=len(High2[0])
+    for i in range(len(High2)):
+        if len(High2[i])<lref:
+            lref=len(High2[i])
+            for j in range(i):
+                High2[j].pop()
+        if len(High2[i])>lref:
+            High2[i].pop()
+
+    return Low1,Low2,High1,High2
